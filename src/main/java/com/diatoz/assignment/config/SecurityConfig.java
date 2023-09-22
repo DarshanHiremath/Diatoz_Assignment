ccf08787aebc13192d7723eb3304b07728e19a80
Darshan Hiremath <87310019+DarshanHiremath@users.noreply.github.com>
2023-09-22 16:01:19 +0530

    Update SecurityConfig.java

--- src/main/java/com/diatoz/assignment/config/SecurityConfig.java
+++ src/main/java/com/diatoz/assignment/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.diatoz.assignment.config;
+
 import com.diatoz.assignment.filter.JwtFilter;
 import com.diatoz.assignment.service.AdminService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
